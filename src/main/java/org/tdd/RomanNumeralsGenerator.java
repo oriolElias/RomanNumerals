d90c014fa8469d9f2c96798155e1f5e04a16330c
oriolElias <Oriol.Elias@gft.com>
2022-09-27 11:33:23 +0000

    refactor and removing duplication

--- src/main/java/org/tdd/RomanNumeralsGenerator.java
+++ src/main/java/org/tdd/RomanNumeralsGenerator.java
@@ -3,19 +3,31 @@ package org.tdd;
 public class RomanNumeralsGenerator {
     public String romanFor(int decimal) {
         String roman = "";
-        if(decimal>=10){
-            roman += "X";
-            decimal -=10;
+
+        for (RomanToDecimal romanToDecimal: RomanToDecimal.values()) {
+            if(decimal>=romanToDecimal.decimal){
+                roman += romanToDecimal.roman;
+                decimal -=romanToDecimal.decimal;
+            }
         }
-        if(decimal >= 5){
-            roman += "V";
-            decimal -= 5;
-        }
+
+
         for (int i = 0; i < decimal; i++) {
             roman += "I";
         }
 
 
         return roman;
+    }
+
+    enum RomanToDecimal{
+        TEN("X",10),
+        FIVE("V",5);
+        private final String roman;
+        private final int decimal;
+        RomanToDecimal(String roman, int decimal){
+            this.roman = roman;
+            this.decimal = decimal;
+        }
     }
 }
